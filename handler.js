cb6e07c524c04d32aa18888938ad8d41f62596d2
Rachel ✨ <rachel.ajayi@thetrainline.com>
2023-11-24 16:24:22 +0000

    connecting to Amazon SES

--- handler.js
+++ handler.js
@@ -1,6 +1,28 @@
 'use strict';
+// top of file
+const AWS = require('aws-sdk');
+const ses = new AWS.SES({ region: 'us-east-1' });
 
 module.exports.sendEmail = async (event) => {
+  // inside the `exports.handler.sendHello` function, before the `return` statement
+const params = {
+  Destination: {
+    ToAddresses: ['rachel-ajayi@hotmail.co.uk'], // This should be your email address
+  },
+  Message: {
+    Body: {
+      Text: {
+        Data: 'This is a message generated automatically from a Lambda function.',
+      },
+    },
+    Subject: {
+      Data: 'Hello from Lambda',
+    },
+  },
+  Source: 'rachel-ajayi@hotmail.co.uk', // This is the email listed in sender. Set it to your email for this practice
+};
+await ses.sendEmail(params).promise();
+// in the object that is `return`ed, replace the `body.message` property with `Email sent to ${queryParams.email}`
   return {
     statusCode: 200,
     body: JSON.stringify(
