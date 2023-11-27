b7585486920ed687b439efd90e2f2731e7af433c
Rachel ✨ <rachel.ajayi@thetrainline.com>
2023-11-27 14:39:43 +0000

    Making Amazon SES dynamic

--- handler.js
+++ handler.js
@@ -1,33 +1,50 @@
-'use strict';
+"use strict";
 // top of file
-const AWS = require('aws-sdk');
-const ses = new AWS.SES({ region: 'us-east-1' });
+const AWS = require("aws-sdk");
+const ses = new AWS.SES({ region: "us-east-1" });
 
 module.exports.sendEmail = async (event) => {
   // inside the `exports.handler.sendHello` function, before the `return` statement
-const params = {
-  Destination: {
-    ToAddresses: ['rachel-ajayi@hotmail.co.uk'], // This should be your email address
-  },
-  Message: {
-    Body: {
-      Text: {
-        Data: 'This is a message generated automatically from a Lambda function.',
+  // select from the query parameters
+  const queryParams = event.queryStringParameters || {};
+  let { email, message, subject } = queryParams;
+
+  message =
+    message ||
+    "This is a message generated automatically from a Lambda function.";
+  subject = subject || "Hello from Lambda";
+
+  if (!email) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Email is required",
+      }),
+    };
+  }
+  const params = {
+    Destination: {
+      ToAddresses: [email], // This should be your email address
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: message,
+        },
+      },
+      Subject: {
+        Data: subject,
       },
     },
-    Subject: {
-      Data: 'Hello from Lambda',
-    },
-  },
-  Source: 'rachel-ajayi@hotmail.co.uk', // This is the email listed in sender. Set it to your email for this practice
-};
-await ses.sendEmail(params).promise();
-// in the object that is `return`ed, replace the `body.message` property with `Email sent to ${queryParams.email}`
+    Source: email, // This is the email listed in sender. Set it to your email for this practice
+  };
+  await ses.sendEmail(params).promise();
+  // in the object that is `return`ed, replace the `body.message` property with `Email sent to ${queryParams.email}`
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: `Email sent to ${email} with subject ${subject} and message ${message}`,
         input: event,
       },
       null,
